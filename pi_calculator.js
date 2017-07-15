7e2b1fba60db17aba0123afea3e1ce7a9b073844
Myke Agonia <myke.agonia@gmail.com>
2017-07-15 17:27:00 +0800

    Fix zero error when someone backspaces zero it goes to medium value

--- pi_calculator.js
+++ pi_calculator.js
@@ -4,29 +4,19 @@ $(function(){
     $('#field1').rangeslider({
         polyfill:false,
         onInit:function(){
-        
             $('#input1').val($('input[type="range"]').val());
         },
-        onSlide:function(position, value){
-            //console.log('onSlide');
-            //console.log('position: ' + position, 'value: ' + value);
-            $('.content #input1').val(value);
-        },
-        onSlideEnd:function(position, value){
-            //console.log('onSlideEnd');
-            //console.log('position: ' + position, 'value: ' + value);
+        // Change the value of input field when slider slides
+        onSlide:function(){
+            $('#input1').val(this.value);
+            console.log('$'+$('#input1').val());
+            ga('send', 'event', 'PI Calculator', 'Slider-Medical-Expenses', 'PI Calculator Change Medical Expenses');
         }
     });
-    // Change the value of input field when slider changes
-    $('#field1').on('input', function() {
-        $('#input1').val(this.value);
-        console.log('$'+$('#input1').val());
-    });
-
     // Change the value of slider field when input changes
     $('#input1').on('input', function() {
-        if (this.value.length == 0) $('#field1').val(0).change();
-
+        if (this.value.length == 0) { $('#field1').val(0).change(); } 
+        ga('send', 'event', 'PI Calculator', 'Input-Medical-Expenses', 'PI Calculator Input Medical Expenses');
         $('#field1').val(this.value).change();
     });
   
@@ -34,77 +24,57 @@ $(function(){
     $('#field2').rangeslider({
         polyfill:false,
         onInit:function(){
-        
             $('#input2').val($('input[type="range"]').val());
         },
-        onSlide:function(position, value){
-            $('.content #input2').val(value);
-        },
-        onSlideEnd:function(position, value){
+        // Change the value of input field when slider slides
+        onSlide:function(){
+            $('#input2').val(this.value);
+            console.log('$'+$('#input2').val());
+            ga('send', 'event', 'PI Calculator', 'Slider-Vehicle-Damage', 'PI Calculator Change Vehicle Damage');
         }
-    });
-    // Change the value of input field when slider changes
-    $('#field2').on('input', function() {
-        $('#input2').val(this.value);
-        console.log('$'+$('#input2').val());
     });
     // Change the value of slider field when input changes
     $('#input2').on('input', function() {
-        if (this.value.length == 0) $('#field2').val(0).change();
-
+        if (this.value.length == 0) { $('#field2').val(0).change(); }
+        ga('send', 'event', 'PI Calculator', 'Input-Vehicle-Damage', 'PI Calculator Input Vehicle Damage');
         $('#field2').val(this.value).change();
     });
-    
-
 
 
     $('#field3').rangeslider({
         polyfill:false,
         onInit:function(){
-        
             $('#input3').val($('input[type="range"]').val());
         },
-        onSlide:function(position, value){
-            $('.content #input3').val(value);
-        },
-        onSlideEnd:function(position, value){
+        onSlide:function(){
+            $('#input3').val(this.value);
+            console.log('$'+$('#input3').val());
+            ga('send', 'event', 'PI Calculator', 'Slider-Lost-Income', 'PI Calculator Change Lost Income');
         }
-    });
-    // Change the value of input field when slider changes
-    $('#field3').on('input', function() {
-        $('#input3').val(this.value);
-        console.log('$'+$('#input3').val());
     });
     // Change the value of slider field when input changes
     $('#input3').on('input', function() {
-        if (this.value.length == 0) $('#field3').val(0).change();
-
+        if (this.value.length == 0) { $('#field3').val(0).change(); }
+        ga('send', 'event', 'PI Calculator', 'Input-Lost-Income', 'PI Calculator Input Lost Income');
         $('#field3').val(this.value).change();
     });
-
 
 
     $('#field4').rangeslider({
         polyfill:false,
         onInit:function(){
-        
             $('#input4').val($('input[type="range"]').val());
         },
-        onSlide:function(position, value){
-            $('.content #input4').val(value);
-        },
-        onSlideEnd:function(position, value){
+        onSlide:function(){
+            $('#input4').val(this.value);
+            console.log('$'+$('#input4').val());
+            ga('send', 'event', 'PI Calculator', 'Slider-Rental-Vehicle-Cost', 'PI Calculator Change Rental Vehicle Cost');
         }
-    });
-    // Change the value of input field when slider changes
-    $('#field4').on('input', function() {
-        $('#input4').val(this.value);
-        console.log('$'+$('#input4').val());
     });
     // Change the value of slider field when input changes
     $('#input4').on('input', function() {
-        if (this.value.length == 0) $('#field4').val(0).change();
-
+        if (this.value.length == 0) { $('#field4').val(0).change(); }
+        ga('send', 'event', 'PI Calculator', 'Input-Rental-Vehicle-Cost', 'PI Calculator Input Rental Vehicle Cost');
         $('#field4').val(this.value).change();
     });
 
@@ -124,5 +94,5 @@ function settlement(form){
   $('#low').text("$"+low_settlement.toLocaleString());
   $('#high').text("$"+high_settlement.toLocaleString());
   
-  
+  ga('send', 'event', 'PI Calculator', 'Button-Calculate-Settlement', 'PI Calculator Click Calculate Settlement Button');
 }
