d5bf9401e3bfeecf3ecb6437d2e7a24130b23f20
Myke Agonia <myke.agonia@gmail.com>
2017-07-15 17:32:08 +0800

    Remove event tracking

--- pi_calculator.js
+++ pi_calculator.js
@@ -10,13 +10,11 @@ $(function(){
         onSlide:function(){
             $('#input1').val(this.value);
             console.log('$'+$('#input1').val());
-            ga('send', 'event', 'PI Calculator', 'Slider-Medical-Expenses', 'PI Calculator Change Medical Expenses');
         }
     });
     // Change the value of slider field when input changes
     $('#input1').on('input', function() {
         if (this.value.length == 0) { $('#field1').val(0).change(); } 
-        ga('send', 'event', 'PI Calculator', 'Input-Medical-Expenses', 'PI Calculator Input Medical Expenses');
         $('#field1').val(this.value).change();
     });
   
@@ -30,13 +28,11 @@ $(function(){
         onSlide:function(){
             $('#input2').val(this.value);
             console.log('$'+$('#input2').val());
-            ga('send', 'event', 'PI Calculator', 'Slider-Vehicle-Damage', 'PI Calculator Change Vehicle Damage');
         }
     });
     // Change the value of slider field when input changes
     $('#input2').on('input', function() {
         if (this.value.length == 0) { $('#field2').val(0).change(); }
-        ga('send', 'event', 'PI Calculator', 'Input-Vehicle-Damage', 'PI Calculator Input Vehicle Damage');
         $('#field2').val(this.value).change();
     });
 
@@ -49,13 +45,11 @@ $(function(){
         onSlide:function(){
             $('#input3').val(this.value);
             console.log('$'+$('#input3').val());
-            ga('send', 'event', 'PI Calculator', 'Slider-Lost-Income', 'PI Calculator Change Lost Income');
         }
     });
     // Change the value of slider field when input changes
     $('#input3').on('input', function() {
         if (this.value.length == 0) { $('#field3').val(0).change(); }
-        ga('send', 'event', 'PI Calculator', 'Input-Lost-Income', 'PI Calculator Input Lost Income');
         $('#field3').val(this.value).change();
     });
 
@@ -68,13 +62,11 @@ $(function(){
         onSlide:function(){
             $('#input4').val(this.value);
             console.log('$'+$('#input4').val());
-            ga('send', 'event', 'PI Calculator', 'Slider-Rental-Vehicle-Cost', 'PI Calculator Change Rental Vehicle Cost');
         }
     });
     // Change the value of slider field when input changes
     $('#input4').on('input', function() {
         if (this.value.length == 0) { $('#field4').val(0).change(); }
-        ga('send', 'event', 'PI Calculator', 'Input-Rental-Vehicle-Cost', 'PI Calculator Input Rental Vehicle Cost');
         $('#field4').val(this.value).change();
     });
 
@@ -94,5 +86,4 @@ function settlement(form){
   $('#low').text("$"+low_settlement.toLocaleString());
   $('#high').text("$"+high_settlement.toLocaleString());
   
-  ga('send', 'event', 'PI Calculator', 'Button-Calculate-Settlement', 'PI Calculator Click Calculate Settlement Button');
 }
